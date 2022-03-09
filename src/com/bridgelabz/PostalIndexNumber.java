f6f1e8da36fbb8b06039edd9fa7c96036303ee04
pragnakasthruri <kpragna25@gmail.com>
2022-03-09 11:15:39 +0530

    Adding the code for printing welcome message

---
+++ src/com/bridgelabz/PostalIndexNumber.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+/**
+ * Creating a PostalIndexNumber class to print welcome message
+ */
+public class PostalIndexNumber {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Postal Index Number Validation");
+    }
+}
